1b8951dc035b33bf2e8c2c063221c4c58b95f300
Jesús Hernández <cjesus02hdz@gmail.com>
2020-08-02 23:33:40 -0500

    Carbon.now.sh iframe add

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -121,3 +121,121 @@ fetch("/api/v1/data")
 		function ocultar(){
 			datosAdetalle.innerHTML= '';
 		}
+
+/* ************ S E C C I Ó N   A P I ************** */
+/* --------- MOSTRAR CÓDIGO PARA HACER GET --------- */
+
+// - Activa el botón desplegable
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.dropdown-trigger');
+	var instances = M.Dropdown.init(elems);
+});
+
+// - Código GET - DATA
+
+function getDataJS() {
+	getData.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=var%2520requestOptions%2520%253D%2520%257B%250A%2520%2520method%253A%2520%27GET%27%252C%250A%2520%2520redirect%253A%2520%27follow%27%252C%250A%257D%250A%250Afetch(%27https%253A%252F%252Fwww.juquila.info%252Fapi%252Fv1%252Fdata%27%252C%2520requestOptions)%250A%2520%2520.then((response)%2520%253D%253E%2520response.text())%250A%2520%2520.then((result)%2520%253D%253E%2520console.log(result))%250A%2520%2520.catch((error)%2520%253D%253E%2520console.log(%27error%27%252C%2520error))"
+  style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+function getDATAPython() {
+	getData.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=import%2520http.client%250Aimport%2520mimetypes%250Aconn%2520%253D%2520http.client.HTTPSConnection(%2522www.juquila.info%2522)%250Apayload%2520%253D%2520%27%27%250Aheaders%2520%253D%2520%257B%257D%250Aconn.request(%2522GET%2522%252C%2520%2522%252Fapi%252Fv1%252Fdata%2522%252C%2520payload%252C%2520headers)%250Ares%2520%253D%2520conn.getresponse()%250Adata%2520%253D%2520res.read()%250Aprint(data.decode(%2522utf-8%2522))"
+  style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+// - Código GET - POSITIVOS
+
+function getDataPositivosJs() {
+	getDataPositivos.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=var%2520requestOptions%2520%253D%2520%257B%250A%2520%2520method%253A%2520%27GET%27%252C%250A%2520%2520redirect%253A%2520%27follow%27%252C%250A%257D%250A%250Afetch(%27https%253A%252F%252Fwww.juquila.info%252Fapi%252Fv1%252Fdata%252Fpositivos%27%252C%2520requestOptions)%250A%2520%2520.then((response)%2520%253D%253E%2520response.text())%250A%2520%2520.then((result)%2520%253D%253E%2520console.log(result))%250A%2520%2520.catch((error)%2520%253D%253E%2520console.log(%27error%27%252C%2520error))"
+  style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+function getDataPositivosPython() {
+	getDataPositivos.innerHTML = `
+	<iframe
+	src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=import%2520http.client%250Aimport%2520mimetypes%250Aconn%2520%253D%2520http.client.HTTPSConnection(%2522www.juquila.info%2522)%250Apayload%2520%253D%2520%27%27%250Aheaders%2520%253D%2520%257B%257D%250Aconn.request(%2522GET%2522%252C%2520%2522%252Fapi%252Fv1%252Fdata%252Fpositivos%2522%252C%2520payload%252C%2520headers)%250Ares%2520%253D%2520conn.getresponse()%250Adata%2520%253D%2520res.read()%250Aprint(data.decode(%2522utf-8%2522))"
+	style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+	sandbox="allow-scripts allow-same-origin">
+  </iframe>
+	`
+}
+// - Código GET - NEGATIVOS
+
+function getDataNegativosJs() {
+	getDataNegativos.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=var%2520requestOptions%2520%253D%2520%257B%250A%2520%2520method%253A%2520%27GET%27%252C%250A%2520%2520redirect%253A%2520%27follow%27%252C%250A%257D%250A%250Afetch(%27https%253A%252F%252Fwww.juquila.info%252Fapi%252Fv1%252Fdata%252Fnegativos%27%252C%2520requestOptions)%250A%2520%2520.then((response)%2520%253D%253E%2520response.text())%250A%2520%2520.then((result)%2520%253D%253E%2520console.log(result))%250A%2520%2520.catch((error)%2520%253D%253E%2520console.log(%27error%27%252C%2520error))"
+  style="width: 100%; height: 340px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+function getDataNegativosPython() {
+	getDataNegativos.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=import%2520http.client%250Aimport%2520mimetypes%250Aconn%2520%253D%2520http.client.HTTPSConnection(%2522www.juquila.info%2522)%250Apayload%2520%253D%2520%27%27%250Aheaders%2520%253D%2520%257B%257D%250Aconn.request(%2522GET%2522%252C%2520%2522%252Fapi%252Fv1%252Fdata%252Fnegativos%2522%252C%2520payload%252C%2520headers)%250Ares%2520%253D%2520conn.getresponse()%250Adata%2520%253D%2520res.read()%250Aprint(data.decode(%2522utf-8%2522))"
+  style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+// - Código GET - SOSPECHOSOS
+
+function getDataSospechososJs() {
+	getDataSospechosos.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=var%2520requestOptions%2520%253D%2520%257B%250A%2520%2520method%253A%2520%27GET%27%252C%250A%2520%2520redirect%253A%2520%27follow%27%252C%250A%257D%250A%250Afetch(%27https%253A%252F%252Fwww.juquila.info%252Fapi%252Fv1%252Fdata%252Fsospechosos%27%252C%2520requestOptions)%250A%2520%2520.then((response)%2520%253D%253E%2520response.text())%250A%2520%2520.then((result)%2520%253D%253E%2520console.log(result))%250A%2520%2520.catch((error)%2520%253D%253E%2520console.log(%27error%27%252C%2520error))"
+  style="width: 100%; height: 340px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+function getDataSospechososPython() {
+	getDataSospechosos.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=import%2520http.client%250Aimport%2520mimetypes%250Aconn%2520%253D%2520http.client.HTTPSConnection(%2522www.juquila.info%2522)%250Apayload%2520%253D%2520%27%27%250Aheaders%2520%253D%2520%257B%257D%250Aconn.request(%2522GET%2522%252C%2520%2522%252Fapi%252Fv1%252Fdata%252Fsospechosos%2522%252C%2520payload%252C%2520headers)%250Ares%2520%253D%2520conn.getresponse()%250Adata%2520%253D%2520res.read()%250Aprint(data.decode(%2522utf-8%2522))"
+  style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+// - Código GET - DEFUNCIONES
+
+function getDataDefuncionesJs() {
+	getDataDefunciones.innerHTML = `
+	<iframe
+  src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=var%2520requestOptions%2520%253D%2520%257B%250A%2520%2520method%253A%2520%27GET%27%252C%250A%2520%2520redirect%253A%2520%27follow%27%252C%250A%257D%250A%250Afetch(%27https%253A%252F%252Fwww.juquila.info%252Fapi%252Fv1%252Fdata%252Fdefunciones%27%252C%2520requestOptions)%250A%2520%2520.then((response)%2520%253D%253E%2520response.text())%250A%2520%2520.then((result)%2520%253D%253E%2520console.log(result))%250A%2520%2520.catch((error)%2520%253D%253E%2520console.log(%27error%27%252C%2520error))"
+  style="width: 100%; height: 340px; border:0; transform: scale(1); overflow:hidden;"
+  sandbox="allow-scripts allow-same-origin">
+</iframe>
+	`
+}
+
+function getDataDefuncionesPython() {
+	getDataDefunciones.innerHTML = `
+	<iframe
+	src="https://carbon.now.sh/embed?bg=rgba(171%2C184%2C195%2C0)&t=seti&wt=none&l=javascript&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fl=1&fm=Hack&fs=13.5px&lh=142%25&si=false&es=2x&wm=false&code=import%2520http.client%250Aimport%2520mimetypes%250Aconn%2520%253D%2520http.client.HTTPSConnection(%2522www.juquila.info%2522)%250Apayload%2520%253D%2520%27%27%250Aheaders%2520%253D%2520%257B%257D%250Aconn.request(%2522GET%2522%252C%2520%2522%252Fapi%252Fv1%252Fdata%252Fdefunciones%2522%252C%2520payload%252C%2520headers)%250Ares%2520%253D%2520conn.getresponse()%250Adata%2520%253D%2520res.read()%250Aprint(data.decode(%2522utf-8%2522))"
+	style="width: 100%; height: 321px; border:0; transform: scale(1); overflow:hidden;"
+	sandbox="allow-scripts allow-same-origin">
+  </iframe>
+	`
+}
